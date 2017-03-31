57134e7fe6feb993c430bdb3878f9631176fa0ad
Jesse Florig <jesseflorig@gmail.com>
2017-03-31 10:04:08 -0400

    WIP loading ships

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const client = new Lokka({
 });
 
 // set timezone to UTC for Graphcool
-process.env.TZ = 'UTC';
+process.env.TZ = 'UTC'
 
 // git clone git@github.com:guidokessels/xwing-data.git
-const rawShips = new jsonloader('../xwing-data/data/ships.js');
-const rawPilots = new jsonloader('../xwing-data/data/pilots.js');
+const rawShips = new jsonloader('../xwing-data/data/ships.js')
+const rawPilots = new jsonloader('../xwing-data/data/pilots.js')
 
 const uniqFlatMap = (data, key) => {
   return chain(data)
@@ -24,10 +24,38 @@ const uniqFlatMap = (data, key) => {
     .value()
 }
 
+const allShips = uniqFlatMap(rawShips, 'name')
 const allFactions = uniqFlatMap(rawShips, 'faction')
 const allActions = uniqFlatMap(rawShips, 'actions')
 const allSlots = uniqFlatMap(rawPilots, 'slots')
 
-console.log('all factions:',allFactions);
-console.log('all actions:',allActions);
-console.log('all slots:',allSlots);
+console.log(`all ships: ${allShips}!`)
+// console.log('all factions:',allFactions)
+// console.log('all actions:',allActions)
+// console.log('all slots:',allSlots)
+
+// create ships
+// const shipIdMap = await createShips(allShips)
+// console.log(`Created ${Object.keys(shipIdMap).length} ships.`)
+
+// const createShips = async (rawShips: Ship[]): Promise<IdMap> => {
+//   const shipIds = await Promise.all(rawShips.map(createShip))
+//   return _.zipObject<IdMap> (rawShips.map(ship => ship.id), shipIds)
+// }
+
+// const createShip = async (ship: Ship): Promise<string> => {
+//   const result = await client.mutate(`{
+//     ship: createShip(
+//       oldId: ${ship.id},
+//       name: "${ship.name}",
+//       attack: ${ship.attack},
+//       agility: ${ship.agility},
+//       hull: ${ship.hull},
+//       shields: ${ship.shields},
+//       size: "${ship.size}",
+//       xws: "${ship.xws}",
+//     )
+//   }`)
+//
+//   return result.ship.id
+// }
