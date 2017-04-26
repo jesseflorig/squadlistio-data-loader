969fa4ecec8636194da1571c7eb686d8b0795616
Jesse Florig <jesseflorig@gmail.com>
2017-04-26 14:21:41 -0400

    fixed slots

--- pilots.js
+++ pilots.js
@@ -2,10 +2,11 @@ import {zipObject, chain, map, filter} from 'lodash'
 
 // create pilots
 const createPilot = async (pilot: Pilot, client: Client) => {
-  const {id: oldId, name, unique = false, skill, points, image, text='', faction, xws} = pilot
+  const {id: oldId, name, unique = false, skill, points, image, slots=[], text="", faction, xws} = pilot
 
   const skillFix = typeof skill == 'number' ? skill : 0
   const pointsFix = typeof points == 'number' ? points : 0
+  const slotsFix = slots.map((slot)=> `"${slot}"`)
 
   const result = await client.mutate(`{
     pilot: createPilot(
@@ -15,6 +16,7 @@ const createPilot = async (pilot: Pilot, client: Client) => {
       skill: ${skillFix},
       points: ${pointsFix},
       image: "${image}",
+      slots: [${slotsFix.toString()}],
       text: "${text.replace(/"/g, '&quote;')}",
       faction: "${faction}",
       xws: "${xws}"
@@ -24,10 +26,10 @@ const createPilot = async (pilot: Pilot, client: Client) => {
   }`)
 
   if(!result) {
-    console.log(pilot)
+    console.log(`ERROR adding ${name}`)
   }
   else {
-    console.log(`Added ${name}`)
+    console.log(`ADDED ${name}`)
   }
 
   return result.pilot.id
@@ -47,38 +49,6 @@ const findPilot = async (pilot: Pilot, client: Client) => {
 
   return result
 }
-
-export const connectPilotsAndSlots = async(rawPilots, newPilots, rawSlots, newSlots, client) => {
-  return await map(rawPilots, async(pilot) => {
-    const newPilotId = newPilots[pilot.id]
-    const newSlotIds = chain(rawSlots)
-      .filter(slot => {
-        return pilot.slots.includes(slot.name)
-      })
-      .map(slot => {
-        return newSlots[slot.name]
-      })
-      .value()
-
-    return await map(newSlotIds, async(newSlotId) => {
-      const connectedPilot = await connectPilotsAndSlotsMutation(newPilotId, newSlotId, client)
-      return connectedPilot
-    })
-  })
-}
-
-const connectPilotsAndSlotsMutation = async(pilotId, slotId, client) => {
-  console.log(`${slotId} -> ${pilotId}`)
-  const result = await client.mutate(`{
-    addToPilotOnSlot(slotsSlotId:"${slotId}" pilotsPilotId:"${pilotId}") {
-      slotsSlot {
-        id
-      }
-    }
-  }`)
-  return result
-}
-
 
 export const createPilots = async (rawPilots: Pilot[], client: Client): Promise<IdMap> => {
   const pilotIds = await Promise.all(rawPilots.map( async(pilot) => {
